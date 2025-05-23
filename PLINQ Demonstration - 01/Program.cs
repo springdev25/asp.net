a49d424d9348617e2dbfcd6f58b9a325bdd4db36
springdev25 <thinhthworkspace@gmail.com>
2025-05-23 16:25:33 +0700

    PLINQ Demo

---
+++ PLINQ Demonstration - 01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+class Program
+{
+    public static void Main()
+    {
+        var range = Enumerable.Range(1, 1000_00);
+        //Here is Sequential version
+        var resultList = range.Where(i => i % 3 == 0).ToList();
+        Console.WriteLine($"Sequential: Total items are: {resultList.Count}");
+        //Here is Parallel version using .AsParallel method
+        var parallelResultList = range.AsParallel().Where(i => i % 3 == 0).ToList();
+        Console.WriteLine($"Parallel: Total items are: {parallelResultList.Count}");
+        resultList = (from i in range.AsParallel() where i % 3 == 0 select i).ToList();
+        Console.WriteLine($"Parallel: Total items are: {resultList.Count}");
+        Console.ReadLine();
+    }//end Main
+}//end Program
